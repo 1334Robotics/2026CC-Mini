0e3677d0640c35e7cb3430a7378dd3c9e2783ccb
aesom-e <114953191+aesom-e@users.noreply.github.com>
2026-02-26 14:58:23 -0500

    Added some more magic number tags

--- src/main/java/frc/robot/commands/ManualDriveCommand.java
+++ src/main/java/frc/robot/commands/ManualDriveCommand.java
@@ -18,6 +18,7 @@ import frc.robot.subsystems.Swerve;
 import frc.util.DriveInputSmoother;
 import frc.util.ManualDriveInput;
 import frc.util.Stopwatch;
+import frc.util.annotations.MagicNumber;
 
 /**
  * Teleop manual drive command for the swerve drivetrain.
@@ -33,7 +34,7 @@ public class ManualDriveCommand extends Command {
         DRIVING_WITH_LOCKED_HEADING
     }
 
-    private static final Time kHeadingLockDelay = Seconds.of(0.25); // time to wait before locking heading
+    @MagicNumber private static final Time kHeadingLockDelay = Seconds.of(0.25); // time to wait before locking heading
 
     private final Swerve swerve;
     private final DriveInputSmoother inputSmoother;
